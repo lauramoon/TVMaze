3090b0f82578b31e1cc2a2f7f9b00d33a70008e1
Laura Moon <laura.kolb@gmail.com>
2021-01-29 09:24:55 -0600

    fix test

--- tvmaze-test.js
+++ tvmaze-test.js
@@ -2,7 +2,7 @@ it('should retrieve list of shows from the api with given search term', async ()
     let bletchleyShows = await searchShows('bletchley');
     expect(bletchleyShows.length).toEqual(2);
     expect(bletchleyShows[0].id).toEqual(1767);
-    expect(bletchleyShows[0].image).toEqual("https://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg");
+    expect(bletchleyShows[0].image).toEqual("http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg");
     expect(bletchleyShows[0].name).toEqual("The Bletchley Circle");
     expect(bletchleyShows[0].summary.length).toBeGreaterThan(100);
 });
